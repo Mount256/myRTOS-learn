77d496fcb3d59c0beaab53d1cf977f82d1d916f1
Mount256 <zluo256@163.com>
2025-08-21 20:53:04 +0800

    完成软件定时器实验

--- 11.软件定时器实验/User/APP/app.c
+++ 11.软件定时器实验/User/APP/app.c
@@ -37,6 +37,7 @@ static CPU_STK   AppTaskTmrStk  [ APP_TASK_TMR_STK_SIZE ];
 */
 static void AppTaskStart (void *p_arg);               
 static void  AppTaskTmr  (void *p_arg);
+void TmrCallback (OS_TMR *p_tmr, void *p_arg);
 
 /*
 *************************************************************************
@@ -146,10 +147,52 @@ static  void  AppTaskTmr  ( void * p_arg )
 	OS_TMR      my_tmr;
     (void)      p_arg;
 
+      
+	/* 创建软件定时器 */
+	OSTmrCreate ((OS_TMR              *)&my_tmr,             // 软件定时器对象
+				 (CPU_CHAR            *)"MySoftTimer",       // 命名软件定时器
+                 (OS_TICK              )10,                  // 定时器初始值(dly)，依10Hz时基(1 tick)计算，即1s(=10*100ms)
+                 (OS_TICK              )10,                  // 定时器周期重载值(period)，依10Hz时基(1 tick)计算，即1s(=10*100ms)
+                 (OS_OPT               )OS_OPT_TMR_PERIODIC, // 工作模式设置为周期性定时
+                 (OS_TMR_CALLBACK_PTR  )TmrCallback,         // 回调函数指针
+                 (void                *)"Timer Over!",       // 传递实参给回调函数
+                 (OS_ERR              *)err);                // 返回错误类型
+					 
+	/* 启动软件定时器 */						 
+	OSTmrStart ((OS_TMR   *)&my_tmr, // 软件定时器对象
+                (OS_ERR   *)err);    // 返回错误类型
+				 
+	ts_start = OS_TS_GET();                       // 获取定时前时间戳
+					 
     while (DEF_TRUE) // 任务体，通常写成一个死循环
-	{      
-		
-    }
+	{    
+		OSTimeDly ( 1000, OS_OPT_TIME_DLY, &err ); // 不断阻塞该任务
+	}
 }
 
+/*
+************************************************************************
+*                          TIMER CALLBACK
+***********************************************************************
+*/
+void TmrCallback (OS_TMR *p_tmr, void *p_arg) // 软件定时器my_tmr的回调函数
+{
+	CPU_INT32U   cpu_clk_freq;	
+	CPU_SR_ALLOC();      
+	
+	printf ("%s", (char *)p_arg);
+	
+	cpu_clk_freq = BSP_CPU_ClkFreq();                   // 获取CPU时钟，时间戳是以该时钟计数
+	
+	LED1_REVERSE; 
+	
+	ts_end = OS_TS_GET() - ts_start;     // 获取定时后的时间戳（以CPU时钟进行计数的一个计数值），并计算定时时间
+	                                     
+	OS_CRITICAL_ENTER();                 
+	printf ("\r\n定时1s，通过时间戳测得定时 %07d us，即 %04d ms。\r\n", 
+						ts_end / ( cpu_clk_freq / 1000000 ),     // 将定时时间折算成 us 
+						ts_end / ( cpu_clk_freq / 1000 ) );      // 将定时时间折算成 ms 
+	OS_CRITICAL_EXIT();                               
 
+	ts_start = OS_TS_GET();                            // 获取定时前时间戳
+}
